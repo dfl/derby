5a28d5a45a5b84ebe1393972736b473558608841
David Lowenfels <david@internautdesign.com>
2012-03-23 18:07:01 -0700

    tweak

--- app.rb
+++ app.rb
@@ -7,16 +7,30 @@ set :database, ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/db/development.sqli
 
 require './models/vote'
 
-# use Rack::Auth::Basic, "Restricted Area" do |username, password|
-#   [username, password] == ['admin', 'admin']
-# end
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  [username, password] == ['admin', 'dallas']
+end
+
+helpers do
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'admin']
+  end
+end
 
 get "/" do
   "OK"
 end
 
 get "/total" do
-  Vote.total.to_json
+  Vote.total.to_json rescue "no votes yet"
 end
 
 post "/vote" do
@@ -25,5 +39,6 @@ post "/vote" do
 end
 
 post "/reset" do
-  Vote.reset_all! #if params[:password]="dallas"
+  protected!
+  Vote.reset_all!
 end
