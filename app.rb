09c31a7284e70afdbd5334f930f58d2acc1e03a5
David Lowenfels <david@internautdesign.com>
2012-03-23 18:08:11 -0700

    fix authentication

--- app.rb
+++ app.rb
@@ -7,10 +7,6 @@ set :database, ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/db/development.sqli
 
 require './models/vote'
 
-use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  [username, password] == ['admin', 'dallas']
-end
-
 helpers do
   def protected!
     unless authorized?
@@ -21,7 +17,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'admin']
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'dallas']
   end
 end
 
